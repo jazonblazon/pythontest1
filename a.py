a7dac5ea64bdb23ed38622169d092b664e7394c4
Jazon Blazon <jbelec@toh.ca>
2018-03-25 15:32:51 -0400

    test1

---
+++ a.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 18:57:36 2018
+
+@author: Jason
+"""
+import numpy
+import pydicom
+
+import b
+
+
+a2=b.b2(2.) #2.+1.+2.+5.= 10.
+print('firsting',a1,a2)
+
+a2=b.b1(2.) #2.+1.+2.+5.= 10.
+print('second',a1,a2)
---
+++ b.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 18:57:36 2018
+
+@author: Jason
+"""
+import c
+
+def b1(var1):
+    a=var1+1.
+    a=a+c.c1(var1)
+    return a
+    
+def b2(var1):
+    a=var1+1.
+    return a
+    
+    
